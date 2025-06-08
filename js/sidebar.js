93ca9d6cd5281993c05b3bddc25d31896384da75
Gustavo Pereira <gustavopereira15102007@gmail.com>
2025-06-08 13:31:17 -0300

    sidebar

---
+++ js/sidebar.js
@@ -0,0 +1,4 @@
+document.querySelector(".sidebar-toggler").addEventListener("click", () => {
+    // Toggle collapsed class on sidebar
+    document.querySelector(".sidebar").classList.toggle("collapsed");
+})
